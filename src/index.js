94b259473dc3f910b5c8bb930f7688fd42d6c5dd
jbadan <jenna.badanowski@gmail.com>
2017-12-29 19:07:17 -0800

    reconfig for redux

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import './assets/stylesheets/styles.scss';
 
 import React from 'react'; // import the main react dependency
 import ReactDOM from 'react-dom'; // import reactDOM
+import { Provider } from 'react-redux';
 import App from './app/App'; // import the main app component
+import Store from './Store';
 
-ReactDOM.render(<App />, document.getElementById('root')); // render our App component and mount it to our #root element
+ReactDOM.render(
+  <Provider store={Store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')); // render our App component and mount it to our #root element
